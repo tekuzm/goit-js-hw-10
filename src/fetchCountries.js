ddc27ebb28d09173c52fb61d9087cbb60f16b6ef
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-10 19:25:13 +0200

    Add styles

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,5 @@
-// const BASE_URL = ``;
-
 export default function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+  const url = `https://restcountries.com/v3.1/name/${name}`;
 
   return fetch(url)
     .then(response => {
@@ -14,3 +12,5 @@ export default function fetchCountries(name) {
       return data;
     });
 }
+
+// якщо 1 країна у результатах пошуку, то до url + ?fields=name,capital,population,flags,languages
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ refs.inputField.addEventListener(
 function inputChangeHandler(event) {
   const name = this.value;
 
+  // якщо поле інпут пусте, очищуй результати пошуку
   if (!name) {
     clearCountryList();
     return;
@@ -27,14 +28,15 @@ function inputChangeHandler(event) {
     .then(countriesArray => {
       clearCountryList();
 
+      // якщо 1 країна у результатах пошуку, то створюй картку країни
       if ((countriesArray.length = 1)) {
-        createCountryCard();
+        // createCountryCard();
       }
 
+      // якщо більш, ніж 1 країна у результатах пошуку, то показуй список країн
       countriesArray.forEach(country => {
-        const listItemEl = document.createElement('li');
-        const countryName = country.name.common;
-        listItemEl.innerHTML = countryName;
+        const listItemEl = `<li class="country-item"><img src="${country.flags.svg}" alt="Flag" width="30", height="20"><span class="country-name">${country.name.official}</span></li>`;
+        refs.countryList.innerHTML = listItemEl;
         refs.countryList.appendChild(listItemEl);
       });
     })
@@ -47,7 +49,7 @@ function clearCountryList() {
   refs.countryList.innerHTML = '';
 }
 
-function createCountryCard() {
-  clearCountryList();
-  refs.countryInfo.innerHTML += `<div class="country-card"><div class="country-header"><img src="${flags.svg}" class="flag-img" alt="Flag" width=55 height=35><h2 class="country-name">${name}</h2></div><p class="country-description">Capital: <span class="country-description--value">${capital}</span></p><p class="country-description">Population: <span class="country-description--value">${population}</span></p><p class="country-description">Languages: <span class="country-description--value">${languages}</span></p></div>`;
+function createCountryCard(countryObj) {
+  const country = countryObj[0];
+  refs.countryInfo.innerHTML = `<div class="country-card"><div class="country-header"><img src="${country.flags.svg}" class="flag-img" alt="Flag" width=55 height=35><h2 class="country-card--name">${country.name.official}</h2></div><p class="country-description">Capital: <span class="country-description--value">${country.capital}</span></p><p class="country-description">Population: <span class="country-description--value">${country.population}</span></p><p class="country-description">Languages: <span class="country-description--value">${country.languages}</span></p></div>`;
 }
